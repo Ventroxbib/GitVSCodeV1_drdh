6313b170253beee3d76ddc5fd8d3d1560a9a6798
bbm3h20ahs <daniel.hasanov@edu.bib.de>
2022-09-06 12:36:43 +0200

    aa

--- sweGithubPrj/src/Presenter.java
+++ sweGithubPrj/src/Presenter.java
@@ -1,5 +1,22 @@
-import Model;
-import View;
-public class Presenter{
 
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Presenter implements ActionListener{
+
+    private View view;
+    private Model model;
+
+    public Presenter(){
+        this.model = new Model();
+        this.view = new View();
+        view.addListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        view.setText(model.getText().toString());
+        
+    }
 }
--- sweGithubPrj/src/View.java
+++ sweGithubPrj/src/View.java
@@ -1,23 +1,6 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+public class View{
 
-public class View extends JFrame {
-
-    private JButton helloWorld;
-
-    public void View(){
-        JFrame meinJFrame = new JFrame();
-        meinJFrame.setTitle("Hello World");
-        JPanel panel = new JPanel();
-        JButton button = new JButton("Hello World");
-
-        panel.add(button);
-
-        meinJFrame.add(panel);
-
-        meinJFrame.pack();
-
-        meinJFrame.setVisible(true);
+    public void addListener(Presenter presenter) {
     }
+
 }
