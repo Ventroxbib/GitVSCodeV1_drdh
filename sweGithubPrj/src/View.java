5d89bf402d02ec984a207eb3f6accdc6dc4bec22
bbm3h20ahs <daniel.hasanov@edu.bib.de>
2022-09-06 12:18:51 +0200

    Comit

--- sweGithubPrj/src/View.java
+++ sweGithubPrj/src/View.java
@@ -1,3 +1,19 @@
-public class View{
-    
+public class View extends JFrame {
+
+    private JButton helloWorld;
+
+    public void View(){
+        JFrame meinJFrame = new JFrame();
+        meinJFrame.setTitle("Hello World");
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Hello World");
+
+        panel.add(button);
+
+        meinJFrame.add(panel);
+
+        meinJFrame.pack();
+
+        meinJFrame.setVisible(true);
+    }
 }
